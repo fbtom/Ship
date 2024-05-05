ae503ad34b2e4d40b72c5c3f9acc980adea510b1
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-05-05 20:39:13 +0200

    Introduce getPrice in Item class

--- item.cpp
+++ item.cpp
@@ -1,1 +1,20 @@
 #include "item.hpp"
+
+size_t Item::getPrice() const
+{
+    auto price{base_price_};
+    if (rarity_ == Rarity::Legendary)
+    {
+        price = price * 2;
+    }
+    else if (rarity_ == Rarity::Epic)
+    {
+        price = price * 1.6;
+    }
+    else if (rarity_ == Rarity::Rare)
+    {
+        price = price * 1.2;
+    }
+
+    return price;
+}
